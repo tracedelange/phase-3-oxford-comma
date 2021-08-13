41e863796a8ad0cbea8816bd87b9acf8c1e015c8
Trace <tracedelange@me.com>
2021-08-12 19:47:40 -0500

    commit tree 4459efe4b8ce0b29fc2d8415e694e0517b4513ca

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+    joined = array.join(', ')
+    comma_count = joined.count(',')
+
+    if comma_count == 1
+        puts joined.split(',').join(' and')
+        return joined.split(',').join(' and')
+    elsif comma_count == 0
+        return joined
+    else
+        return joined.reverse.sub(',','dna ,').reverse        
+    end
 end
